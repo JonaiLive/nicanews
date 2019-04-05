8b4b94415fccc2b41907b1c5f601d671c61f5714
Jonai Soto <jonailive@gmail.com>
2019-04-04 21:10:50 -0400

    add styling to webapp

--- main.js
+++ main.js
@@ -3,12 +3,10 @@ window.addEventListener("load", () => {
   var url =
     "https://newsapi.org/v2/everything?" +
     "q=nicaragua& ONLY" +
-    "from=2019-04-02&" +
-    "sortBy=relevancy&" +
-    "language=es&";+
-    "API_KEY";
-
-  const API_KEY = process.env.API_KEY;
+    "from=2019-04-04&" +
+    "sortBy=publishedAt&" +
+    "language=es&" +
+    "apiKey=myKey";
 
   var req = new Request(url);
 
@@ -19,11 +17,17 @@ window.addEventListener("load", () => {
       let output = "";
       data.articles.forEach(function(articles) {
         output += `
-          <ul>
-          <li>${articles.title}</li>
-          <li>${articles.url}</li>
+      <div class="articles">
+        <ul>
+          <li><p class="artTitle">${articles.title}</p></li>
+          <li><img src="${articles.urlToImage}"></li>
+          // <li><a href="${
+            articles.url
+          }"><button type="button">Ver Articulo</button></a></li>
+
           <br>
           </ul>
+        </div>
         `;
       });
       document.getElementById("news-articles").innerHTML = output;
